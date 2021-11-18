66533545ff6b6d19b299bcb23ec71b288cd0358a
Taweechai Yuenyang <krumii.it@gmail.com>
2021-11-18 16:14:31 +0700

    เพิ่มการบันทึกข้อมูลด้วย sqllite

--- main.py
+++ main.py
@@ -8,12 +8,28 @@ env_path = f"{app_path}/.env"
 load_dotenv(env_path)
 
 y = Yazaki()
-sql = sqlite3.connect("sync.db")
+conn = sqlite3.connect(f"data/{os.getenv('DB_LITE_NAME')}.db")
+
 
 def main():
     doc = y.get_gedi()
     if doc != False:
-        print("success")
+        cur = conn.cursor()
+        # Create table
+        cur.execute(
+            """create table if not exists gedi_files(objtype,mailbox,batchid,size,batchfile,currentdate,flags,formats,orgname,factory,filetype,download,linkfile)"""
+        )
+        i = 0
+        while i < len(doc):
+            # Insert a row of data
+            cur.execute(
+                "INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)"
+            )
+            i += 1
+
+        # Save (commit) the changes
+        conn.commit()
+        conn.close()
         return
 
     print("error load")
