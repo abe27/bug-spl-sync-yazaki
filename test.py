6ff55737a64075f62eb98f8fc085d62b0ced5355
Taweechai Yuenyang <krumii.it@gmail.com>
2021-11-22 09:03:25 +0700

    ทดสอบ Fast Api

--- test.py
+++ test.py
@@ -6,6 +6,24 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
+class GediModel(BaseModel):
+    id: int  # integer primary key autoincrement,
+    objtype: str  # text not null,
+    mailbox: str  # text not null,
+    batchid: str  # text not null unique,
+    size: float  # real,
+    batchfile: str  # text,
+    currentdate: int  # numeric,
+    flags: str  # text,
+    formats: str  # text,
+    orgname: str  # text,
+    factory: str  # text,
+    filetype: str  # text,
+    download: int  # integer,
+    destination: str  # text,
+    linkfile: str  # text)"""
+
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
